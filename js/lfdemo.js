5bfbca92d4a65f66cc3858f270c6d813cb0fb2de
chenjiajun <845142388@qq.com>
2016-01-20 22:14:04 +0800

    init

--- js/lfdemo.js
+++ js/lfdemo.js
@@ -14,24 +14,24 @@
             this.createArea();
         },
         createMap:function(){
-            this.map = new BMap.Map('map');
-            var poi = new BMap.Point(116.307852,40.057031);
-            this.map.centerAndZoom(poi, 16);
-            this.map.enableScrollWheelZoom();
-            //this.map.disableDragging();
-           /* this.map.addEventListener("click",function(e){
-                console.log(e.point.lng + "," + e.point.lat);
+            this.point = [51.505, -0.09];
+            this.map = L.map('map').setView(this.point, 13);
+            var cloudmadeUrl = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png';
+            var subDomains = ['otile1','otile2','otile3','otile4'];
+            var cloudmadeAttrib = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
+            L.tileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttrib, subdomains: subDomains}).addTo(this.map);
+            /*this.map.on('click',function(e){
+                console.log(e.latlng);
             });*/
+
         },
         createArea:function(){
-            this.mySquare = new SquareOverlay(this.map.getCenter(), 200, "rgba(0,0,0,0.5)",this.map);
-            this.map.addOverlay(this.mySquare);
-            //console.log(JSON.stringify(this.getGeography()));
+            this.mySquare = new SquareOverlay(this.point, 200, "rgba(0,0,0,0.5)",this.map);
         },
         getGeography:function(){
             return this.mySquare.getGeography();
         }
     }
-    // 百度地图API功能
+    //地图API功能
     mapManager.init();
 })();
